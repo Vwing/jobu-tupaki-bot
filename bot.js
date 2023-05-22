6f6867fc0344bb43aeda360647eada3676553ff5
Vincent Wing <vincentwing00@gmail.com>
2023-05-21 23:21:14 -0700

    added error handling, fixed sliding context window

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const tmi = require('tmi.js');
 const { Configuration, OpenAIApi } = require("openai");
 const https = require('https');
-const fs = require('fs'); // Import fs module for reading files
-const {encode} = require('gpt-3-encoder'); // Import encode function
+const fs = require('fs');
+const {encode} = require('gpt-3-encoder');
 
 const oauthToken = process.env.TWITCH_ACCESS_TOKEN;
 
@@ -43,7 +43,7 @@ function createBot(token) {
 	  const commandName = msg.trim();
 
 	  if (commandName == '!jobu reset'){
-		  resetChat();
+		  resetChat(target);
 	  }
 	  else if (commandName.startsWith('!jobu')) {
 		await handleChat(target, commandName.slice(5).trim());
@@ -54,7 +54,7 @@ function createBot(token) {
 	  console.log(`* Connected to ${addr}:${port}`);
 	}
 
-	async function resetChat() {
+	async function resetChat(target) {
 		chatHistory = [];
 		chatHistory.push({role: "system", content: "A user has done the \"!jobu reset\" command, which is when your memory is reset to just the first system message. Any quippy remarks you'd like to add to the default \"{memory reset}\" message?"});
 		chatHistory.push({role: "assistant", content: "Jobu: Ah, the old \"!jobu reset\" command, a classic way to wipe the slate clean and plunge us back into the beautiful chaos of the unknown. Well, my friend, with a flick of my multiversal wrist and a hearty chuckle, I present to you..."});
@@ -82,22 +82,36 @@ function createBot(token) {
 	}
 
 	function pruneChatHistory() {
-		while (encode(chatHistory.map(m => m.content).join(' ')).length > 4000-encode(systemMessage)) {
+		const systemMessageLength = encode(systemMessage).length;
+		while (encode(chatHistory.map(m => m.content).join(' ')).length > 4000-systemMessageLength) {
 		  chatHistory.shift();
 		}
 	}
 
 	async function fetchChatBotResponse(maxTokens) {
+	  console.log("fetching chat bot response for chat history: ");
+	  console.log(chatHistory);
+	  try {
 		const response = await openai.createChatCompletion({
 		  model: "gpt-3.5-turbo",
 		  messages: [
-		    {role: "system", content: systemMessage}, // Load system message from file
-		    ...chatHistory // Spread the chat history into the messages array
+			{role: "system", content: systemMessage},
+			...chatHistory
 		  ],
 		  max_tokens: maxTokens
 		});
 
 		return response.data.choices[0].message.content;
+	  } catch (error) {
+		console.error('Error occurred when fetching chat bot response:', error.message);
+
+		if (error.response && error.response.data && error.response.data.error) {
+		  console.error('Details:', error.response.data.error.message);
+		  console.error('Request ID:', error.response.headers['x-request-id']);
+		}
+
+		return error.message;
+	  }
 	}
 }
 
